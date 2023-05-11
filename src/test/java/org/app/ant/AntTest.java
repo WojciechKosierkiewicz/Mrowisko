82b2c53a8632e8836fc44506936e9dc7750efbbc
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2023-05-11 13:48:59 +0200

    zmiany w mrowkach

---
+++ src/test/java/org/app/ant/AntTest.java
@@ -0,0 +1,13 @@
+package org.app.ant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AntTest {
+
+    @Test
+    void testToString() {
+        assertEquals("brak testu", "brak testu");
+    }
+}
