4de68d8a53b5f2c446f72e3b9218fd5320c938cd
Igor <272897@student.pwr.edu.pl>
2023-05-12 17:50:04 +0200

    ant zmiany igor
    (cherry picked from commit 347ff0c974252b658aecf6adb8b9729636d24ee9)

--- src/main/java/org/app/ant/Ant.java
+++ src/main/java/org/app/ant/Ant.java
@@ -10,6 +10,9 @@ public class Ant extends Agent {
     private double antHunger;
     private double antDecisionFactor;
 
+    private double antAngle;
+
+
     UUID id_mrowiska;
 
     public Ant() {
@@ -18,15 +21,29 @@ public class Ant extends Agent {
         this.antDecisionFactor = 0.5;
     }
 
-    public Ant(double antHunger, double antDecisionFactor, UUID id_mrowiska) {
+    public Ant(double antHunger, double antDecisionFactor, UUID id_mrowiska, int x, int y) {
         super();
         this.antHunger = antHunger;
         this.antDecisionFactor = antDecisionFactor;
         this.id_mrowiska = id_mrowiska;
+        this.setLocx(x);
+        this.setLocy(y);
     }
 
+
     public int getLivedUpdates() {
+
         return livedUpdates;
+    }
+
+    public double getLocXofAnt() {
+
+        return getLocx();
+    }
+
+    public double getLocYOfAnt() {
+
+        return getLocy();
     }
 
     private void setAntHunger() {
@@ -42,17 +59,29 @@ public class Ant extends Agent {
         return antHunger;
     }
 
-    private double getAntDecisionFactor(){
+    private double getAntDecisionFactor() {
 
         return antDecisionFactor;
     }
+
+    public UUID getId_mrowiska() {
+
+        return id_mrowiska;
+    }
+
 
     private boolean moveTest() {
 
         return true;
     }
 
-    private void moveAnt(){
+    private double countAntAngle() {
+
+
+        return antAngle;
+    }
+
+    private void moveAnt() {
 
     }
 
